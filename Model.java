02008839e7de64e3de8e279f044cd9dd7dbe93b7
Josh DeSieno <35925723+joshdesieno@users.noreply.github.com>
2020-08-25 15:48:17 -0400

    Update Model.java

--- Model.java
+++ Model.java
@@ -18,11 +18,11 @@ public class Model implements KeyListener {
 
 	private ArrayList<DrawChar> onScreen = new ArrayList<DrawChar>(); 
 	private char [] alphanums = 
-						{'a','b','c','d','e','f','g','h','i','j','k','l','m', 
-				         'n','o','p','q','r','s','t','u','v','w','x','y','z',	 
-				         'A','B','C','D','E','F','G','H','I','J','K','L','M', 
-				  		 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',    
-				 		 '0','1','2','3','4','5','6','7','8','9'};
+				{'a','b','c','d','e','f','g','h','i','j','k','l','m', 
+				 'n','o','p','q','r','s','t','u','v','w','x','y','z',	 
+				 'A','B','C','D','E','F','G','H','I','J','K','L','M', 
+				 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',    
+				 '0','1','2','3','4','5','6','7','8','9'};
 
 	public Model() {
 		timer = new Timer(timerMS, new ActionListener() {
